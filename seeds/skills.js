e1c2646c11f277b6076933a0cabc712d44921a80
Erwin Hessing <ejhessing@Erwins-MacBook-Pro.local>
2016-09-09 20:59:30 +1200

    Add first set of seeds to the Tables

--- seeds/skills.js
+++ seeds/skills.js
@@ -5,9 +5,27 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('skills').insert({id: 101, skillName: 'Change Lightbulb', category: 'Domestic'}),
-        knex('skills').insert({id: 102, skillName: 'How to Write a Cheque', category: 'Budget'}),
-        knex('skills').insert({id: 103, skillName: 'How to Grow Herbs', category: 'Outside'})
-      ])
+        // knex('skills').insert({id: 101, skillName: 'Change Lightbulb', category: 'Domestic'}),
+        // knex('skills').insert({id: 102, skillName: 'How to Write a Cheque', category: 'Budget'}),
+        // knex('skills').insert({id: 103, skillName: 'How to Grow Herbs', category: 'Outside'}),
+        knex('skills').insert({id: 104, skillName: 'How to Cook Scrambled Eggs', category: 'Cooking'}),
+        knex('skills').insert({id: 105, skillName: 'How to Fry Eggs', category: 'Cooking'}),
+        knex('skills').insert({id: 106, skillName: 'How to Poach Eggs Perfectly', category: 'Cooking'}),
+
+        knex('skills').insert({id: 107, skillName: 'How to Change A Car Tyre', category: 'Auto'}),
+        knex('skills').insert({id: 108, skillName: 'How To Jumpstart Your Car', category: 'Auto'}),
+        knex('skills').insert({id: 109, skillName: 'How to Check Your Tyre Pressure', category: 'Auto'}),
+
+        knex('skills').insert({id: 1010, skillName: 'How to Iron Clothes Properly?', category: 'Domestic'}),
+        knex('skills').insert({id: 1011, skillName: 'How to Change a Light Bulb', category: 'Domestic'}),
+        knex('skills').insert({id: 1012, skillName: 'How to Wash Clothes', category: 'Domestic'}),
+
+        knex('skills').insert({id: 1013, skillName: 'How to Write a Cheque', category: 'Budget'}),
+        knex('skills').insert({id: 1014, skillName: 'How to Write a Budget', category: 'Budget'}),
+        knex('skills').insert({id: 1015, skillName: 'How to Save Money', category: 'Budget'})
+
+
+      ])Auto
     })
+
 }

2b0bb766ee996a13d4acb1a3ceba736b84ed7bfd
Erwin <ejhessing@gmail.com>
2016-09-09 15:02:22 +1200

    Fix types for migrations and seed data for videos

--- migrations/20160909140839_userSkills.js
+++ migrations/20160909140839_userSkills.js
@@ -1,8 +1,8 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('userSkills', (table) => {
     table.increments('id').primary()
-    table.string('skill_id').references('id').inTable('skills').onUpdate('CASCADE').onDelete('CASCADE')
-    table.string('user_id').references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+    table.integer('skill_id').references('id').inTable('skills').onUpdate('CASCADE').onDelete('CASCADE')
+    table.integer('user_id').references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
     table.string('status')
     table.string('showcase')
   })

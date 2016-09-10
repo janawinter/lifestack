33fa3c9493ed2fc21dffe18d475784e6690370d3
amandine-perichon <apericho@gmail.com>
2016-09-10 16:14:36 +1200

    Add mock login route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path')
 const cors = require('cors')
 const PORT = process.env.PORT || 3000
 
+//Remove when done with mock login route
+const db = require('./lib/database')
+
+
 const app = express()
 
 const skills = require('./routes/v1/skills')
@@ -15,5 +19,16 @@ app.use(cors())
 
 app.use('/v1/skills', skills)
 app.use('/v1/users', users)
+
+//Remove when done with mock login route
+app.use('/login', (req, res) => {
+  console.log(req.body.username, req.body.password)
+    let id = 199
+    db.getUserDetails(id)
+      .then((data) => {
+        res.json({data: data})
+      })
+      .catch(() => res.sendStatus(500))
+})
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))

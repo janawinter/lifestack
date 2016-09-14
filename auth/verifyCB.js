6177631dbf03c7f2da64677c5d74852a5f674daa
Erwin <ejhessing@gmail.com>
2016-09-14 18:27:18 +1200

    Remove console logs

--- auth/verifyCB.js
+++ auth/verifyCB.js
@@ -65,23 +65,18 @@ function verify (token, tokenSecret, profile, done) {
     db.getUserById(profile.id)
       .then((user) => {
         if (user[0]) {
-          console.log("User exists")
           return done(null, user)
         } else {
-          console.log("User does not exist")
           db.addUser(profile.id, profile.username, profile.photos[0].value)
             .then((user) => {
-              console.log("User has been created")
               return done(null, user)
             })
             .catch((err) => {
-              console.log("Error adding user in DB")
               return done(err, null)
             })
         }
       })
       .catch((err) => {
-        console.log("Error finding user in DB")
         return done(err, null)
       })
 }

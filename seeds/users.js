604c34bdfab15c15b608727f63af512eb9805e59
Erwin <ejhessing@gmail.com>
2016-09-14 22:14:29 +1200

    Add fake data

---
+++ seeds/users.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('users').insert({id: 199, username: 'Janawhat', twitter_id:'297120112', profile_pic: 'http://www.aspirehire.co.uk/aspirehire-co-uk/_img/profile.svg'}),
+        knex('users').insert({id: 198, username: 'phant35', twitter_id:'473383919', profile_pic: 'http://www.aspirehire.co.uk/aspirehire-co-uk/_img/profile.svg'})
+      ])
+    })
+}
